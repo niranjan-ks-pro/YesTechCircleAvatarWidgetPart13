6a11384e449b779685c35b1ec088be000a5ae030
Niranjan-KS <131178323+niranjan-ks-pro@users.noreply.github.com>
2023-06-28 18:24:03 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# YesTechCircleAvatarWidgetPart13
+ https://www.youtube.com/watch?v=j8-jgYXVyrE&list=PL1JrLEBAapUWv_JSolgE5pdcDKWw80wKA&index=13
---
+++ android/app/src/main/kotlin/com/example/yestechcircleavatarwidgetpart13/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.yestechcircleavatarwidgetpart13
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
